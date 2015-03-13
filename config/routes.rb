3342de6881e4169bbd14ccb1736de6de52c2e471
fokayx <fokayx@gmail.com>
2015-03-13 21:29:38 +0800

    static_pages/service

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :hospitals
   resources :exams
   resources :items
+  get 'static_pages/service'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
