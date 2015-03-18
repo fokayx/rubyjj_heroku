82f158185a96db5bd6aa627e03c30553ef8711cf
fokayx <fokayx@gmail.com>
2015-03-18 11:34:33 +0800

    contact mail flash at index

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,12 +5,10 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver.later
-      flash.now[:notice] = 'Thank YOU'
-      render root_path
+    if @contact.deliver
+      redirect_to root_path, notice: '感謝您的來信, RubyJJ處理中'
     else
-      flash.now[:error] = 'Cannot send message.'
-      render :index
+      render :index, notice: '抱歉，系統錯誤，請稍候再試,謝謝'
     end
   end
 
