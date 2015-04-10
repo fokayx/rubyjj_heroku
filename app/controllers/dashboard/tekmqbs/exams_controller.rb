bc48b7193b0fc2be9defd25751d2f50835982189
fokayx <fokayx@gmail.com>
2015-04-10 17:42:04 +0800

    fix edit missing required keys

--- app/controllers/dashboard/tekmqbs/exams_controller.rb
+++ app/controllers/dashboard/tekmqbs/exams_controller.rb
@@ -15,9 +15,16 @@ class Dashboard::Tekmqbs::ExamsController < Dashboard::Tekmqbs::AdminController
   end
 
   def edit
+    @hospital = Hospital.find(params[:hospital_id])
+    #要多傳一個hospital的參數給網址囉。不對是多從網址多抓一個hospital的參數回來給form
   end
 
   def update
+    if @exam.update(exam_params)
+      redirect_to dashboard_tekmqbs_hospital_path(params[:hospital_id]), notice: '成功修改套餐'
+    else
+      render :edit
+    end
   end
 
   def show
