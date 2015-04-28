453b8d57978b1e9e6bb1fb416c98da96724d5b87
fokayx <fokayx@gmail.com>
2015-04-28 15:21:16 +0800

    add page_title, help / view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(page_title)
+    base_title = "RubyJJ 紅寶石健檢"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_titel} | #{bese_title}"
+    end
+  end
 end
